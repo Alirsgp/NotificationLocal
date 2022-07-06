5285592daf3df5202c9fc48d28927e7dec657035
Ali Mohammadian <alirsgp@gmail.com>
2022-07-06 12:59:51 -0700

    Initial Commit

---
+++ NotificationLocal/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  NotificationLocal
+//
+//  Created by Ali Mohammadian on 7/6/22.
+//
+
+import SwiftUI
+import UserNotifications
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Button("Request Permission") {
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        print("All set!")
+                    } else if let error = error {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+
+            Button("Schedule Notification") {
+                let content = UNMutableNotificationContent()
+                content.title = "Exercise time!"
+                content.subtitle = "Go out for a run, do some push ups"
+                content.sound = UNNotificationSound.default
+
+                // show this notification five seconds from now
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+
+                // choose a random identifier
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+
+                // add our notification request
+                UNUserNotificationCenter.current().add(request)
+            }
+        }
+    }
+}
---
+++ NotificationLocal/NotificationLocalApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationLocalApp.swift
+//  NotificationLocal
+//
+//  Created by Ali Mohammadian on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct NotificationLocalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
